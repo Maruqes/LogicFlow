9ce60f55502e7fde2e36ef9ad59dee62bf9737d4
Maruqes <m4rques1s0n@gmail.com>
2024-12-19 15:38:14 +0000

    testing

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import logicircuit.LCComponent;
 import logicircuit.LCDFrameCmd;
 import logicircuit.LCDPanel;
 
@@ -8,5 +9,17 @@ public class Main {
         LCDFrameCmd frame = new LCDFrameCmd(parser, "LogicFlow", 900, 700);
         LCDPanel drawPannel = frame.drawPanel();
         drawPannel.clear();
+
+        drawPannel.drawComponent(LCComponent.AND, 0, 0);
+        drawPannel.drawComponent(LCComponent.OR, 0, 100);
+        drawPannel.drawComponent(LCComponent.NOT, 0, 200);
+        drawPannel.drawComponent(LCComponent.NAND, 0, 300);
+
+        drawPannel.drawWire(LCComponent.AND, 0, 0, LCComponent.OR, 0, 100, null, true);
+
+        drawPannel.drawComponent(LCComponent.BIT3_DISPLAY, 500, 500, 5);
+        drawPannel.drawComponent(LCComponent.XOR, 0, 400);
+        drawPannel.drawComponent(LCComponent.NOR, 0, 500);
+        
     }
 }
